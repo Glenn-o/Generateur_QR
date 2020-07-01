58921c5135b712ef0449e4d08757ea97df44760c
Glenn Oberlé <oberle.glenn@icloud.com>
2020-07-01 20:13:29 +0200

    Updates

---
+++ precache-manifest.a1dc6578125a9a13d5b71df72e9747cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bca5c77fceedf9a135a931e47984d02",
+    "url": "/Generateur_QR/index.html"
+  },
+  {
+    "revision": "fd0ddb08ef59ddaa37f3",
+    "url": "/Generateur_QR/static/css/main.d7093ee2.chunk.css"
+  },
+  {
+    "revision": "c2067ca90710ecf3072c",
+    "url": "/Generateur_QR/static/js/2.00d6c742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Generateur_QR/static/js/2.00d6c742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd0ddb08ef59ddaa37f3",
+    "url": "/Generateur_QR/static/js/main.473396ff.chunk.js"
+  },
+  {
+    "revision": "e0fedf4593733fe8903c",
+    "url": "/Generateur_QR/static/js/runtime-main.5badffc0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Generateur_QR/static/media/logo.5d5d9eef.svg"
+  }
+]);
